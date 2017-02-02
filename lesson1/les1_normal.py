e53117be3efd470f178e2d2e1f7760c5fc5e10ae
alexYurin <sans.y@mail.ru>
2017-02-03 00:33:28 +0700

    complite les1-3

--- lesson1/les1_normal.py
+++ lesson1/les1_normal.py
@@ -1,18 +1,30 @@
 # lesson1, normal
 
 #1
-'''
 n = int(0)
 num = str(input('Введите целое число: '))
 for i in num:
   if (int(i))>n:
     n=int(i)
 print(n)
-'''
 
 #2
-'''
 a = int(input('Введите a: '))
 b = int(input('Введите b: '))
 print('a =', (a+b)-a, 'b =', (a+b)-b)
-'''
+
+#3
+import math
+print('Дано квадратное уравнение:\nax**2 + bx + c')
+a = float(input('Введите a: '))
+b = float(input('Введите b: '))
+c = float(input('Введите c: '))
+D = b**2-4*a*c
+if D>0:
+    x1 = (-b + math.sqrt(D))/a*2
+    x2 = (-b - math.sqrt(D))/a*2
+    print('Корни уравнения:\n','x1 = ', x1, '\n x2 = ', x2)
+elif D==0:
+    print('Корень уравнения: ', x1)
+else:
+    print('Нет корней')
