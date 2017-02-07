3bb199d216a308c1af0d293815200bf843876e68
alexYurin <sans.y@mail.ru>
2017-02-07 17:53:56 +0700

    edit les2, hard

--- lesson2/les2_hard.py
+++ lesson2/les2_hard.py
@@ -1,10 +1,23 @@
 #lesson2, hard
 
 #1
-equation = 'y = -12x + 11111140.2121'
+import re
+
+# y = kx - b
+equation = 'y = -12x + 67'
 x = 2.5
 
-eq = equation.replace('x', '*{}'.format(x))
-#res = eq(eq.find('='))
+equation = equation.replace(' ', '')
 
-print(eq[eq.find('=')+2:])
+k = (re.compile('[+-]*\d*x+').findall(equation))[0][:-1]
+if not len(k): k = 1
+elif k == '-': k = -1
+else: k = int(k)
+
+b = (re.compile('[+-]?\d*[^x]').findall(equation))
+'''
+y = k * x - b
+
+print('{}\nпри x = {}\ny = {}'.format(equation, x, y))
+'''
+print(b)
