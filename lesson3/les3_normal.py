7a9fd877d13b120c133c953edada5189bf8c20d9
alexYurin <sans.y@mail.ru>
2017-02-15 17:50:06 +0700

    add les3-2, normal

--- lesson3/les3_normal.py
+++ lesson3/les3_normal.py
@@ -1,6 +1,7 @@
 #lesson 3, normal
 
-#1
+#1---------------------------
+'''
 def fibonacci(n, m):
     n -= 1
     fib = [1, 1]
@@ -10,5 +11,18 @@ def fibonacci(n, m):
     del fib
     print(res)
 
-fibonacci(3, 5)
-        
+fibonacci(2, 10)
+'''
+#2---------------------------
+
+def sort_to_max(li):
+    min_elem = float('inf')
+    sort_list = []
+        for i in li:
+            if i < min_elem:
+                min_elem = i
+                sort_list.append(i)
+                li.remove(i)
+    print(sort_list)
+    
+sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
