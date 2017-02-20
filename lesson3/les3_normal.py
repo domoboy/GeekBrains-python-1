1a802e907b385a159eca1e51c70735b9add16888
alexYurin <sans.y@mail.ru>
2017-02-20 12:17:40 +0700

    complete les3-4, normal

--- lesson3/les3_normal.py
+++ lesson3/les3_normal.py
@@ -56,7 +56,6 @@ def alt_filter(func, itr):
     '''
     реализация функции filter
     '''
-    print(type(itr))
     new_itr = [elem for elem in itr if func(elem)]
     if type(itr) is tuple:
         new_itr = tuple(new_itr)
@@ -71,6 +70,44 @@ alt_filter(lambda x: x > 5, {2, 10, -12, 2.5, 20, -11, 4, 4, 0})
 
 #4---------------------------
 
+import math
+
+A1, A2, A3, A4 = (2, 3), (0, 2), (4, 1), (6, 2)
+
+def isparall(a, b, c, d):
+    '''
+    Проверка признаков параллелограмма
+    '''
+    p1 = False
+    p2 = False
+    
+   #Противополжные стороны параллельны и равны
+    ab = math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
+    cb = math.sqrt((b[0] - c[0])**2 + (b[1] - c[1])**2)
+    cd = math.sqrt((d[0] - c[0])**2 + (d[1] - c[1])**2)
+    ad = math.sqrt((d[0] - a[0])**2 + (d[1] - a[1])**2)
+    if ab == cd and cb == ad:
+        print('Равенство сторон: верно')
+        p1 = True
+    else:
+        print('Противоположные стороны НЕ равны')
+    
+   #Диагонали O1 и O2 в точках пересечения делятся пополам и равны
+    hO1 = ((a[0] + c[0])/2, (a[1] + c[1])/2)
+    hO2 = ((b[0] + d[0])/2, (b[1] + d[1])/2)
+    if hO1 == hO2:
+        print('Равенство половин диагоналей: верно')
+        p2 = True
+    else:
+        print('Половины диагоналей НЕ равны')
+
+    if p1 and p2:
+        print('Вершины A1%s, A2%s, A3%s, A4%s\nобразуют параллелограмм'%
+          (a, b, c, d))
+    else:
+        print('Вершины не образуют параллелограмм')
+
+isparall(A1, A2, A3, A4)
 
 
 
