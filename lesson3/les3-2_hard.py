0c280484c615c0512b433f38bc9d1d39b1b36728
alexYurin <sans.y@mail.ru>
2017-02-28 01:33:23 +0700

    edit les3-2, hard

--- lesson3/les3-2_hard.py
+++ lesson3/les3-2_hard.py
@@ -5,14 +5,14 @@ def tolist(f_path):
     with open(f_path, encoding='utf-8') as lister:
         li = [elems for elems in lister]
         li = [[e.strip() for e in elem if len(e)] for
-               elem in [elems.split(' ') for elems in li]]
+              elem in [elems.split('  ') for elems in li]]
         return li
 
 
 persons = tolist('workers.txt')
 hourse = tolist('hourse_of.txt')
+pers_header = persons.pop(0)
+hourse_header = hourse.pop(0)
 
-print(persons)
+print(pers_header)
 print(hourse)
-
-
