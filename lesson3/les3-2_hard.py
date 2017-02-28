e6e67c68e893a9e348397b85bfbfe1f6d347b0bc
alexYurin <sans.y@mail.ru>
2017-02-28 17:51:38 +0700

    edit les3-2, hard

--- lesson3/les3-2_hard.py
+++ lesson3/les3-2_hard.py
@@ -1,18 +1,80 @@
 # lesson3-2, hard
 
 
-def tolist(f_path):
-    with open(f_path, encoding='utf-8') as lister:
-        li = [elems for elems in lister]
-        li = [[e.strip() for e in elem if len(e)] for
-              elem in [elems.split('  ') for elems in li]]
-        return li
+def tolist(path):
+    '''
+    Возвращает массив строк из файла path
+    '''
+    with open(path, encoding='UTF-8') as lister:
+        nlist = [elems for elems in lister]
+        nlist = [[el.strip() for el in elem if len(el)] for
+                 elem in [elems.split(' ') for elems in nlist]]
+        return nlist
 
 
-persons = tolist('workers.txt')
-hourse = tolist('hourse_of.txt')
-pers_header = persons.pop(0)
-hourse_header = hourse.pop(0)
+def couple(header, values):
+    '''
+    Возвращает массив словарей с ключами из
+    header и соответсвующими им значениями из
+    values
+    '''
+    nlist = [list(zip(header, value)) for value in values]
+    nlist = [{elem[0]: elem[1] for elem in elems} for
+              elems in nlist]
+    return nlist
 
-print(pers_header)
-print(hourse)
+
+def merge(file1, file2):
+    '''
+    Возвращает общую таблицу из file1 и file2
+    в виде списка из словарей по каждому сотруднику
+    '''
+    persons_list = tolist(file1)  # Создание списка строк из табл.№1
+    houres_list = tolist(file2)  # Создание списка строк из табл.№2
+    header_p = persons_list.pop(0)  # Выделение заголовка из табл.№1
+    header_h = houres_list.pop(0)  # Выделение заголовка из табл.№2
+    '''
+    Создание пары заголовок - значение для каждого сотрудника
+    в каждой таблице
+    '''
+    personal = couple(header_p, persons_list)
+    hourse = couple(header_h, houres_list)
+    '''
+    Слияние таблиц
+    '''
+    for el in personal:
+        for e in hourse:
+            if (el['Фамилия'] == e['Фамилия'] and
+                el['Имя'] == e['Имя']):
+                el.update(e)
+
+    return personal
+
+def calc_pay(tabl):
+    '''
+    Расчёт зарплаты
+    '''
+    for person in tabl:
+        pay = int(person['Зарплата'])
+        h_need = int(person['Норма_часов'])
+        h_fact = int(person['Отработано_часов'])
+        h_pay = int(pay / h_need)
+
+        if h_fact == h_need:
+            person['Расчёт'] = pay
+        elif h_fact > h_need:
+            person['Расчёт'] = pay + (h_fact - h_need) * (h_pay*2)
+        else:
+            person['Расчёт'] = h_pay * h_fact            
+
+        return tabl                    
+        
+
+personal = calc_pay(merge('workers.txt', 'hourse_of.txt'))
+
+with open('calc_pay.txt', 'w', encoding='UTF-8') as pay_list:
+    pay_list.write(personal[0]['Имя'] + '\n' + personal[0]['Фамилия'])
+
+
+
+
