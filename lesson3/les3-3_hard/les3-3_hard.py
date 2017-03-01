9ed9fae7979183a997f931c1a82ba2bd38483587
alexYurin <sans.y@mail.ru>
2017-03-01 14:31:11 +0700

    Complete les3-3, hard!

---
+++ lesson3/les3-3_hard/les3-3_hard.py
@@ -0,0 +1,31 @@
+# lesson3-3, hard
+
+alphabet = tuple(map(chr, range(ord('А'), ord('Я')+1)))
+
+def extract_fruits(file):
+    '''
+    Возвращает сортированный по алфавиту
+    список фруктов из file
+    '''
+    with open(file, 'r', encoding='UTF-8') as fr:
+        fruits = []
+        fr_all = [fruit.strip() for fruit in fr]
+        fr_all = [fruit for fruit in fr_all if len(fruit)]
+        for letter in alphabet:
+            fr_sorted = [fruit for fruit in fr_all if letter == fruit[0]]
+            if len(fr_sorted):
+                fruits.append({letter: fr_sorted})
+
+    return fruits
+
+def sort_fruits(fr_list):
+    for fruit in fr_list:
+        for key, value in fruit.items():
+            with open('fruits_' + key + '.txt',
+                      'w', encoding='UTF-8') as fr_sort:
+
+                fr_sort.write('\n\n'.join(value))
+
+fruits = extract_fruits('fruits.txt')
+sort_fruits(fruits)
+
