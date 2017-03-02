00c06157c8af0af731dc7f32d5f88ec46aacedcb
alexYurin <sans.y@mail.ru>
2017-03-02 18:03:15 +0700

    edit les4-2, normal

---
+++ lesson4/les4-2_normal.py
@@ -0,0 +1,42 @@
+# lesson 4-2, normal
+
+import re
+
+# 1----------------------------------------
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgA' \
+       'YEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
+       'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawv' \
+       'jPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
+       'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBD'\
+       'WuxIhyjJaXDYwdoVPnsllMngNlmkp' \
+       'YOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrf'\
+       'inzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSA' \
+       'fJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwi'\
+       'imqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAb' \
+       'fCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPF'\
+       'kapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyA' \
+       'xDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjA'\
+       'uDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFN' \
+       'IsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnL'\
+       'fBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUf' \
+       'lwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGs'\
+       'iWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZC' \
+       'nZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxF'\
+       'krRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxy' \
+       'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKh'\
+       'yYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
+       'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyle'\
+       'XylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+pattern1 = '[A-Z][a-z]{2}[A-Z]{2}[A-Z]'
+pattern2 = '[a-z]{2}[A-Z]{2}'
+found = re.findall(pattern1, line)
+found = [' '.join(re.split(pattern2, string)) for string in found]
+
+result1 = ' '.join(found)
+
+print(result1)
+
+# 2----------------------------------------
+
