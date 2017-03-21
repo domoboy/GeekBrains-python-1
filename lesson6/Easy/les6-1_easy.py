9f392ae0f8f33563e966ea582075a0b70685b107
alexYurin <sans.y@mail.ru>
2017-03-21 16:47:12 +0700

    edit les6-1_easy

--- lesson6/Easy/les6-1_easy.py
+++ lesson6/Easy/les6-1_easy.py
@@ -1,48 +1,45 @@
 # lesson6-1, easy
 
-import math
+from math import sqrt
 
 
-class Triangle:
-    def __init__(self, a, b, c):
-        '''
-        Длины сторон треугольника
-        '''
-        try:
-            self.AB = round(math.sqrt((b[0]-a[0])**2 + (b[1]-a[1])**2), 2)
-            self.BC = round(math.sqrt((c[0]-b[0])**2 + (c[1]-b[1])**2), 2)
-            self.CA = round(math.sqrt((a[0]-c[0])**2 + (a[1]-c[1])**2), 2)
-        except TypeError:
-            print('Неверные параметры!')
+class Side:
+    @staticmethod
+    def width(A, B):
+        return round(sqrt(sum(tuple(map(lambda a, b: (b - a)**2,
+                                             A, B)))), 2)
 
-    def perimetr(self):
-        return round(self.AB + self.BC + self.CA, 2)
+
+class Triangle(Side):
+    def __init__(self, A, B, C):
+        self._A = A
+        self._B = B
+        self._C = C
+
+    def sides(self):
+        return {'AB': self.width(self._A, self._B),
+                'BC': self.width(self._B, self._C),
+                'CA': self.width(self._C, self._A)
+                }
+
+    def perimeter(self):
+        return round(self.sides()['AB'] +
+                     self.sides()['BC'] +
+                     self.sides()['CA'], 2)
 
     def area(self):
-        pp = self.perimetr() / 2
-        S = math.sqrt(pp*(pp-self.AB)*(pp-self.BC)*(pp-self.CA))
-        return round(S, 2)
+        return round((lambda pp, x, y, z:
+                      sqrt(pp*(pp - x)*(pp - y)*(pp - z)))
+                      (self.perimeter() / 2,
+                       self.sides()['AB'],
+                       self.sides()['BC'],
+                       self.sides()['CA']), 2)
 
-    @property
-    def getAB(self):
-        return self.AB
+A1, A2, A3 = (2, -5),(-6, 2), (6, -2)
 
-    @property
-    def getBC(self):
-        return self.BC
+ABC = Triangle(A1, A2, A3)
 
-    @property
-    def getCA(self):
-        return self.CA
-
-    @property
-    def getH(self):
-        return 2*self.area() / self.CA
-
-
-tri = Triangle((2, -5), (-6, 1), (6, -2))
-print('В треугольнике со сторонами:\nAB {}cm,\nBC {}cm,\nCA'
-      '{}cm'.format(tri.getAB, tri.getBC, tri.getCA))
-print('Периметр: {} cm'.format(tri.perimetr()))
-print('Площадь: {} кв.cm'.format(tri.area()))
-print('Высота: {} cm'.format(tri.getH))
+print('В треугольнике со сторонами:')
+print('см\n'.join([str(side) for side in ABC.sides().values()]) + 'cм')
+print('Площадь: {}кв.см'.format(ABC.area()))
+print('Периметр: {}см'.format(ABC.perimeter()))
